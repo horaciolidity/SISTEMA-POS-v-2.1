aa0bfa1a8d195e559905f9306f773ec94c83ec88
horaciolidity <horacio.dev.sol@gmail.com>
2025-10-30 00:22:48 -0300

    cambios alineados

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
 import { ThemeProvider } from '@/contexts/ThemeContext';
-
+import { AuthProvider } from '@/contexts/AuthContext';
+import { POSProvider } from '@/contexts/POSContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <AuthProvider>
+        <POSProvider>
+          <App />
+        </POSProvider>
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
