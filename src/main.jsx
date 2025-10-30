f7141ed04c5082bc89a3edb2a531ab658acbb63c
horaciolidity <horacio.dev.sol@gmail.com>
2025-10-30 00:13:37 -0300

    cambios alineados

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
-import App from "./App";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { POSProvider } from "@/contexts/POSContext";
+import App from "./App";
 import "@/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
